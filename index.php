2b82caa35a159659ec5ca97f6f41ae58ab54a8c5
Кирилл <madn123@yandex.ru>
2019-10-27 22:17:33 +0300

    Исправлены недочеты по циклам

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 $user_name = 'Кирилл';
-$cats = ["Доски и лыжи" , "Крепления" , "Ботинки" , "Одежда" , "Инструменты" , "Разное"];
+$categories = ['Доски и лыжи' , 'Крепления' , 'Ботинки' , 'Одежда' , 'Инструменты' , 'Разное'];
 $products = [
 	[
 		'name' => '2014 Rossignol District Snowboard',
@@ -97,16 +97,11 @@ $products = [
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-        	<!-- Я бы хотел сделать вывод категорий через цикл foreach, но не смог понять как через него обращаться к простому массиву -->
-        	<?php
-			$index = 0;
-			$num = count($cats);
-			while ($index < $num): ?>
+        	<?php foreach ($categories as $category): ?>
 		        <li class="promo__item promo__item--boards">
-		            <a class="promo__link" href="pages/all-lots.html"><?=$cats[$index];?></a>
+		            <a class="promo__link" href="pages/all-lots.html"><?=$category; ?></a>
 		        </li>
-		    <?php $index++; ?>
-			<?php endwhile; ?>
+		    <?php endforeach ?>
         </ul>
     </section>
     <section class="lots">
@@ -114,18 +109,18 @@ $products = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-        	<?php foreach ($products as $key => $value): ?>
+        	<?php foreach ($products as $product): ?>
 	            <li class="lots__item lot">
 	                <div class="lot__image">
-	                    <img src="<?=$value['img']; ?>" width="350" height="260" alt="">
+	                    <img src="<?=$product['img']; ?>" width="350" height="260" alt="">
 	                </div>
 	                <div class="lot__info">
-	                    <span class="lot__category"><?=$value['cats']; ?></span>
-	                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['name']; ?></a></h3>
+	                    <span class="lot__category"><?=$product['cats']; ?></span>
+	                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$product['name']; ?></a></h3>
 	                    <div class="lot__state">
 	                        <div class="lot__rate">
 	                            <span class="lot__amount">Стартовая цена</span>
-	                            <span class="lot__cost"><?=$value['price']; ?><b class="rub">р</b></span>
+	                            <span class="lot__cost"><?=$product['price']; ?><b class="rub">р</b></span>
 	                        </div>
 	                        <div class="lot__timer timer">
 	                            12:23
@@ -142,15 +137,11 @@ $products = [
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <?php
-			$index = 0;
-			$num = count($cats);
-			while ($index < $num): ?>
+            <?php foreach ($categories as $category): ?>
 	            <li class="nav__item">
-	                <a href="pages/all-lots.html"><?=$cats[$index];?></a>
+	                <a href="pages/all-lots.html"><?=$category; ?></a>
 	            </li>
-	        <?php $index++; ?>
-			<?php endwhile; ?>
+	        <?php endforeach ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
