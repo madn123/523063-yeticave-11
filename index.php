b669d0dab8d9f619ef483c70532ef9db239edc25
Кирилл <madn123@yandex.ru>
2019-11-10 17:41:00 +0300

    123

--- index.php
+++ index.php
@@ -1,78 +1,55 @@
 <?php
+require_once 'functions.php';
+
 $is_auth = rand(0, 1);
 $user_name = 'Кирилл';
 
 date_default_timezone_set("Europe/Moscow");
 setlocale(LC_ALL, 'ru_RU');
 
-function conver_time($с_time) {
-    $diff = strtotime($с_time) - time();
-    $hours = floor($diff / 60 / 60);
-    $seconds = $diff - ($hours * 60 * 60);
-    $hours = str_pad ($hours, 2, "0", STR_PAD_LEFT);
-    $seconds = floor($seconds / 60);
-    $seconds = str_pad ($seconds, 2, "0", STR_PAD_LEFT);
-    $с_time = $hours . ':' . $seconds;
-    return $с_time;
-}
-
-function edit($price) {
-    $price = ceil($price);
-	$price = number_format($price, 0, '', ' ');
-    $price .= " " . "₽";
-	return $price;
-}
-
-function include_template($name, $data) {
-    $name = 'templates/' . $name;
-    $result = '';
-
-    if (!file_exists($name)) {
-        return $result;
-    }
-
-    ob_start();
-    extract($data);
-    require $name;
-
-    $result = ob_get_clean();
-
-    return $result;
-}
-
 $link = mysqli_connect("localhost", "root", "", "yeticave");
 mysqli_set_charset($link, "utf8");
+
 if (!$link) {
     $error = mysqli_connect_error();
     $content = include_template('error.php', ['error' => $error]);
+    print($content);
+    die();
 }
-else {
-    $sql = 'SELECT id, category_name, category_code FROM categories';
-    $result = mysqli_query($link, $sql);
 
-    if ($result) {
-        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    }
-    else {
-        $error = mysqli_error($link);
-        $content = include_template('error.php', ['error' => $error]);
-    }
-    $sql = 'SELECT name, start_price, image, completion_date, category_name FROM items i'
-         . 'JOIN categories c ON i.category_id = c.id'
-         . 'ORDER BY date_creation ASC DESC LIMIT 9';
+$sql = 'SELECT id, category_name, category_code FROM categories';
 
-    if ($res = mysqli_query($link, $sql)) {
-        $items = mysqli_fetch_all($res, MYSQLI_ASSOC);
-    }
-    else {
-        $content = include_template('error.php', ['error' => mysqli_error($link)]);
-    }
+$result = mysqli_query($link, $sql);
+
+if (!$result) {
+    $error = mysqli_connect_error();
+    $content = include_template('error.php', ['error' => $error]);
+    print($content);
+    die();
 }
+
+$categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+$sql = 'SELECT name, start_price, image, completion_date, category_name FROM items i'
+     . 'JOIN categories c ON i.category_id = c.id'
+     . 'ORDER BY date_creation ASC DESC LIMIT 9';
+
+$res = mysqli_query($link, $sql);
+
+if (!$res) {
+    $error = mysqli_connect_error();
+    $content = include_template('error.php', ['error' => $error]);
+    print($content);
+    die();
+}
+
+$items = mysqli_fetch_all($res, MYSQLI_ASSOC);
 
 $page_content = include_template('main.php', [
 	'categories' => $categories,
 	'items' => $items
 ]);
+
 $layout_content = include_template('layout.php', [
 	'content' => $page_content,
 	'categories' => $categories,
@@ -81,5 +58,6 @@ $layout_content = include_template('layout.php', [
 	'is_auth' => $is_auth,
 	'error' => $error
 ]);
+
 print($layout_content);
 
---
+++ templates/error.php
@@ -0,0 +1,8 @@
+﻿<main class="content">
+    <div class="content__main-col">
+        <header class="content__header">
+            <h2 class="content__header-text">Ошибка</h2>
+            <p class="error"><?= $error; ?></p>
+        </header>
+    </div>
+</main>
