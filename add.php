8b35962e2d03d708bcb69eb746e3b293bfcd511a
Кирилл <madn123@yandex.ru>
2019-11-15 17:37:22 +0300

    Валидация

--- add.php
+++ add.php
@@ -10,48 +10,120 @@ $sql = 'SELECT id, category_name, category_code FROM categories ORDER BY categor
 
 $result = mysqli_query($link, $sql);
 
+$cats_ids = [];
+
 if (!$result) {
     $error = debug_error($link);
 }
 
 $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
+$cats_ids = array_column($categories, 'id');
 
-$page_content = include_template('add-lot.php', [
-    'categories' => $categories,
-    'error' => $error
-]);
-
-$layout_content = include_template('layout.php', [
-	'content' => $page_content,
-	'categories' => $categories,
-	'title' => 'Добавить лот',
-	'user_name' => $user_name,
-	'is_auth' => $is_auth,
-	'error' => $error
-]);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $lot = $_POST;
 
-        $filename = uniqid() . '.jpg';
-        $lot['image'] = $filename;
-        move_uploaded_file($_FILES['lot-img']['tmp_name'], 'uploads/' . $filename);
+    $required = ['category_id', 'name', 'description', 'completion_date', 'start_price', 'step_bet'];
+    $errors = [];
 
-        $sql = 'INSERT INTO items (date_creation, category_id, creator_user_id, name, description, image, completion_date, step_bet) VALUES (NOW(), ?, 1, ?, ?, ?, ?, ?)';
+    $rules = [
+    'category_id' => function($value) use ($cats_ids) {
+        return validateCategory($value, $cats_ids);
+    },
+    'name' => function($value) {
+        return validateLength($value, 10, 128);
+    },
+    'description' => function($value) {
+        return validateLength($value, 10, 1000);
+    },
+    'completion_date' => function($value) {
+        return is_date_valid($value);
+    },            
+    'start_price' => function($value) {
+        return validateLength($value, 1, 11);
+    },
+    'step_bet' => function($value) {
+        return validateLength($value, 1, 11);
+    }
+    ];
 
-        $stmt = db_get_prepare_stmt($link, $sql, $lot);
-        $res = mysqli_stmt_execute($stmt);
+    $lots = filter_input_array(INPUT_POST, [
+        'category_id' => FILTER_DEFAULT,
+        'name' => FILTER_DEFAULT,
+        'description' => FILTER_DEFAULT,
+        'completion_date' => FILTER_DEFAULT,
+        'start_price' => FILTER_DEFAULT,
+        'step_bet' => FILTER_DEFAULT
+    ], true);
 
-        if (!$res) {
-            $error = debug_error($link);
+    foreach ($lots as $key => $value) {
+        if (isset($rules[$key])) {
+            $rule = $rules[$key];
+            $errors[$key] = $rule($value);
         }
 
-        $lot_id = mysqli_insert_id($link);
+        if (in_array($key, $required) && empty($value)) {
+            $errors[$key] = "Поле $key надо заполнить";
+        }
+    }
 
-        header("Location: lot.php?id=" . $lot_id);
+    $errors = array_filter($errors);
+
+    if (!empty($_FILES['image']['name'])) {
+        $tmp_name = $_FILES['image']['tmp_name'];
+        $path = $_FILES['image']['name'];
+        $filename = uniqid() . '.jpg';
+
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $file_type = finfo_file($finfo, $tmp_name);
+        if ($file_type !== "image/jpg") {
+            $errors['file'] = 'Загрузите картинку в формате JPG';
+        }
+        else {
+            move_uploaded_file($tmp_name, __DIR__ . '/uploads/' . $filename);
+            $lots['path'] = $filename;
+        }
+    }
+    else {
+        $errors['file'] = 'Вы не загрузили файл';
+    }
+
+    if (count($errors)) {
+        $page_content = include_template('add-lot.php', [
+            'lots' => $lots, 
+            'errors' => $errors, 
+            'categories' => $categories
+        ]);
+    }
+
+    else {
+        $sql = 'INSERT INTO items (date_creation, category_id, creator_user_id, name, description, image, completion_date, start_price, step_bet) VALUES (NOW(), ?, 1, ?, ?, ?, ?, ?, ?)';
+        $stmt = db_get_prepare_stmt($link, $sql, [
+            $category_id,
+            $name, 
+            $description, 
+            'uploads/' . $filename,
+            $completion_date,
+            $start_price, 
+            $step_bet
+        ]);
+        $res = mysqli_stmt_execute($stmt);
+
+        if ($res) {
+            $lot_id = mysqli_insert_id($link);
+
+            header("Location: add.php?id=" . $lot_id);
+        }
+    }
+}
+else {
+    $page_content = include_template('add-lot.php', ['categories' => $categories]);
 }
 
-print($layout_content);
+$layout_content = include_template('layout.php', [
+    'content'    => $page_content,
+    'categories' => []
+]);
 
+print($layout_content);
 
--- templates/add-lot.php
+++ templates/add-lot.php
@@ -12,25 +12,27 @@
     <form class="form form--add-lot container form--invalid" action="add.php" method="post" enctype="multipart/form-data"> <!-- form--invalid -->
       <h2>Добавление лота</h2>
       <div class="form__container-two">
-
-        <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
+      	<?php $classname = isset($errors['name']) ? "form__item--invalid " : ""; ?>
+        <div class="form__item <?= $classname; ?>"> <!-- form__item--invalid -->
           <label for="lot-name">Наименование <sup>*</sup></label>
-          <input id="lot-name" type="text" name="name" placeholder="Введите наименование лота">
+          <input type="text" name="name" placeholder="Введите наименование лота">
           <span class="form__error">Введите наименование лота</span>
         </div>
 
-        <div class="form__item">
+        <?php $classname = isset($errors['category_id']) ? "form__item--invalid " : ""; ?>
+        <div class="form__item <?= $classname; ?>">
           <label for="category">Категория <sup>*</sup></label>
           <select id="category" name="category_id">
           	<?php foreach ($categories as $category): ?>
-              	<option><?=$category['category_name']; ?></option>
+              	<option value="<?=$category['id']; ?>"><?=$category['category_name']; ?></option>
         	<?php endforeach ?>
           </select>
           <span class="form__error">Выберите категорию</span>
         </div>
       </div>
 
-      <div class="form__item form__item--wide">
+      <?php $classname = isset($errors['description']) ? "form__item--invalid " : ""; ?>
+      <div class="form__item form__item--wide <?= $classname; ?>">
         <label for="message">Описание <sup>*</sup></label>
         <textarea id="message" name="description" placeholder="Напишите описание лота"></textarea>
         <span class="form__error">Напишите описание лота</span>
@@ -39,7 +41,7 @@
       <div class="form__item form__item--file">
         <label>Изображение <sup>*</sup></label>
         <div class="form__input-file">
-          <input class="visually-hidden" type="file" id="lot-img" value="">
+          <input class="visually-hidden" type="file" id="lot-img" name="image" value="">
           <label for="lot-img">
             Добавить
           </label>
@@ -47,26 +49,39 @@
 
       </div>
       <div class="form__container-three">
-        <div class="form__item form__item--small">
+      	<?php $classname = isset($errors['start_price']) ? "form__item--invalid " : ""; ?>
+        <div class="form__item form__item--small <?= $classname; ?>">
           <label for="lot-rate">Начальная цена <sup>*</sup></label>
           <input id="lot-rate" type="text" name="start_price" placeholder="0">
           <span class="form__error">Введите начальную цену</span>
         </div>
 
-        <div class="form__item form__item--small">
+        <?php $classname = isset($errors['step_bet']) ? "form__item--invalid " : ""; ?>
+        <div class="form__item form__item--small <?= $classname; ?>">
           <label for="lot-step">Шаг ставки <sup>*</sup></label>
           <input id="lot-step" type="text" name="step_bet" placeholder="0">
           <span class="form__error">Введите шаг ставки</span>
         </div>
 
-        <div class="form__item">
+        <?php $classname = isset($errors['step_bet']) ? "form__item--invalid " : ""; ?>
+        <div class="form__item <?= $classname; ?>">
           <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
           <input class="form__input-date" id="lot-date" type="text" name="completion_date" placeholder="Введите дату в формате ГГГГ-ММ-ДД">
           <span class="form__error">Введите дату завершения торгов</span>
         </div>
       </div>
 
-      <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
+      
+      <?php if (isset($errors)): ?>
+        <div class="form__errors">
+           <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
+           <ul>
+             <?php foreach ($errors as $val): ?>
+                <li><strong><?= $val; ?>:</strong></li>
+             <?php endforeach; ?>
+           </ul>
+        </div>
+     <?php endif; ?>
       <button type="submit" class="button">Добавить лот</button>
     </form>
   </main>
